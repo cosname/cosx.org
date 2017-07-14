93fb2b8293547df731b0269d7d80357baa334fb3
Yihui Xie <xie@yihui.name>
2017-07-14 15:17:22 -0500

    太岁头上清理代码

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,45 +1,34 @@
 (function() {
 
-function show_iframe(iframe_elem) {
-    if (iframe_elem.hasAttribute("src")) {
-        return;
-    }
-    iframe_elem.src = iframe_elem.getAttribute("data-src");
-    iframe_elem.style.height = "700px";
-    iframe_elem.style.visibility = "visible";
+function show_iframe(el) {
+  if (el.hasAttribute("src")) return;
+  el.src = el.getAttribute("data-src");
+  el.style.height = "700px";
+  el.style.visibility = "visible";
+}
+// https://stackoverflow.com/a/488073/559676
+function on_screen(el) {
+  var rect = el.getBoundingClientRect();
+  return (rect.top >= 0) && (rect.bottom <= window.innerHeight);
 }
 
-// https://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling/488073#488073
-function on_screen(elem) {
-    var elem_top = elem.getBoundingClientRect().top;
-    var elem_bot = elem.getBoundingClientRect().bottom;
-    var on_scr = (elem_top >= 0) && (elem_bot <= window.innerHeight);
-    return on_scr;
-}
-
-function iframe_delayed_loading(iframe_elem) {
-    // If the element is already on the current screen, start loading it
-    if (on_screen(iframe_elem)) {
-        show_iframe(iframe_elem);
-    } else {
-        // Otherwise, attach the action to the scroll event
-        function listener() {
-            if (on_screen(iframe_elem)) {
-                // Run only once
-                document.removeEventListener("scroll", listener);
-                show_iframe(iframe_elem);
-            }
-        }
-        document.addEventListener("scroll", listener);
-    }
+function iframe_delayed_loading(el) {
+  // If the element is already on the current screen, start loading it
+  if (on_screen(el)) return show_iframe(el);
+  // Otherwise, attach the action to the scroll event
+  function listener() {
+    if (!on_screen(el)) return;
+    // Run only once
+    document.removeEventListener("scroll", listener);
+    show_iframe(el);
+  }
+  document.addEventListener("scroll", listener);
 }
 
 // Find all iframes
-var iframes = document.querySelectorAll("iframe[data-src]");
-var len = iframes.length;
-var i;
-for (i = 0; i < len; i++) {
-    iframe_delayed_loading(iframes[i]);
+var iframes = document.querySelectorAll("iframe[data-src]"), len = iframes.length;
+for (var i = 0; i < len; i++) {
+  iframe_delayed_loading(iframes[i]);
 }
 
 })();
