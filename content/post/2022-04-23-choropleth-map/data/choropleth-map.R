b5a8faaf79c7415afbaa7b518d116cf9d0c99871
Xiangyun Huang <xiangyunfaith@outlook.com>
2022-05-22 10:37:42 +0800

    投稿：地区分布图及其应用 黄湘云 (#1027)
    * 专题地图与 R 语言及其应用
    
    * use optipng -o5
    
    * 用 https://tinypng.com 继续优化 MB 级的图片
    
    * tweak texts
    
    * use xfun::tinify to optimize png files
    
    * polish
    
    * tweak text
    
    * 调整文本
    
    * 添加文章目录
    
    * 继续抛光
    
    * tweak
    
    * 如此这般调整癌症死亡率
    
    * add blogdown
    
    * 北卡收入分布全宽展示
    
    * 应用全宽方案
    
    * 小调整
    
    * 补充人口年龄结构图的代码
    
    * 让全宽图的 caption 居中
    
    * 删掉一些感情色彩强烈的经验总结
    
    * 继续清理有情绪的部分
    
    * cleanup
    
    * 继续清理和很小的代码修正
    
    * reorg
    
    * 优化 CI Rank 图，用 `geom_pointrange` 替代 `geom_segment`，但 ggplot2 似乎有不能排序的 BUG
    
    * 尝试安装开发版 ggplot2 解决，但是只解决了图例方向问题，详见 https://github.com/tidyverse/ggplot2/issues/4732
    
    * 补充优化 CI 图片
    
    * 要使用分组排序 reorder 其实不是 ggplot2 的 BUG
    
    * tweak text
    
    * 补充 leaflet 绘图代码
    
    * tweak code
    
    * 既然有很多至少也添加两个技术方案
    
    * 7 其它表达细节
    
    * 全文涉及美国的「城镇」都改为「郡」，「专题地图」改为「地区分布图」，这样通俗合理一些
    
    * 默认图层和符号图层对比
    
    * 家庭收入中位数 -- 家庭月收入
    
    * 添加 lattice 绘图步骤
    
    * 再加一点 7 种方案的技术总结
    
    * reorg code
    
    * 在 Ubuntu 下竟可以用 tidycensus 直接下载数据，故而重新组织代码和数据，大瘦身 :rocket:
    
    * 去掉全宽图形
    
    * 修复北卡社区级家庭月收入的空间分布图，之前手动从网站下载的地图数据有些微的问题，即 ACS 调查的年份 2019 和地图数据的年份 2020 不一致
    
    * 美国各个郡的家庭月收入
    
    * 添加各州、郡家庭月收入数据
    
    * 忽略缓存目录
    
    * 添加宽格式和长格式的展示
    
    * 万里长征快走完了
    
    * 更新社区级二元地区分布图
    
    * 莫兰指数刻画空间相关性，获取具体的城镇，以便后续解读数据
    
    * 重画家庭月收入图，添加计算莫兰指数的 R 包说明
    
    * 莫兰指数地图
    
    * Polish
    
    * 最后提及交互式图形，引一下两篇个人博客，以示了解
    
    * 在二元地图中，缺失数据的社区以白色块填充
    
    * 交待一下空间上有部分社区的数据缺失的原因
    
    * * 是家庭年收入，统一修改，交待数据指标的实际含义
    
    * 交待用到的地图数据
    
    * cleanup
    
    * 白人占比也在空间集聚
    
    * 根据审稿人意见修改文本，添加论坛 ID
    
    * tweak text
    
    * update date and cleanup
    
    * 代码中家庭月收入 --> 家庭年收入
    
    * tweak
    
    * tweak text
    
    * minor update
    
    * 更新日期 2022-05-13
    
    * 继续清理未使用到的代码、超宽代码等
    
    * 一些文本编辑和格式化

---
+++ content/post/2022-04-23-choropleth-map/data/choropleth-map.R
@@ -0,0 +1,74 @@
+library(tidycensus)
+library(sf)
+options(tigris_use_cache = TRUE)
+Sys.setenv(CENSUS_API_KEY="YOUR_KEY")
+
+# -------------------------------------------------
+# 北卡家庭年收入和白人数量
+# -------------------------------------------------
+## 郡级
+nc_income_race_county <- get_acs(
+  state = "NC",
+  geography = "county",
+  variables = c("B19013_001", "B02001_001E", "B02001_002E"),
+  geometry = TRUE, output = "wide",
+  year = 2019, survey = "acs5",
+  moe_level = 90
+)
+## 社区级
+nc_income_race_tract <- get_acs(
+  state = "NC",
+  geography = "tract",
+  variables = c("B19013_001", "B02001_001E", "B02001_002E"),
+  geometry = TRUE, output = "wide",
+  year = 2019, survey = "acs5",
+  moe_level = 90
+)
+## 查看郡级收入分布
+plot(nc_income_race_county["B19013_001E"])
+## 查看社区级收入分布
+plot(nc_income_race_tract["B19013_001E"])
+
+### 社区级家庭年收入的空间分布
+showtext::showtext_auto()
+par(mar = c(2, 0, 0, 0))
+plot(nc_income_race_tract["B19013_001E"],
+     key.pos = 1,
+     pal = viridisLite::plasma, reset = FALSE,
+     breaks = c(0:10, 15, 20, 25) * 10000,
+     border = "gray80", main = "", lwd = 0.1
+)
+### 添加郡边界
+plot(st_geometry(nc_income_race_county), border = "gray", lwd = 0.25, add = TRUE)
+mtext(text = "数据源：美国人口调查局", side = 1, adj = 0.05)
+
+## 保存数据
+saveRDS(nc_income_race_county, file = "Desktop/nc_income_race_county.rds")
+saveRDS(nc_income_race_tract, file = "Desktop/nc_income_race_tract.rds")
+
+# -------------------------------------------------
+# 北卡地图数据
+# -------------------------------------------------
+## NC 社区普查级地图数据
+nc_tract_map <- tigris::tracts(state = 37, cb = T, year = 2019, class = "sf")
+## NC 郡级地图数据
+nc_county_map <- tigris::counties(state = 37, cb = T, year = 2019, class = "sf")
+## 保存地图数据
+saveRDS(nc_county_map, file = "Desktop/nc_county_map.rds")
+saveRDS(nc_tract_map, file = "Desktop/nc_tract_map.rds")
+
+# -------------------------------------------------
+# 2019 年美国州、郡地图数据 比例尺 1:20 million
+# -------------------------------------------------
+# 州级地图数据
+us_state_map <- tigris::states(cb = T, year = 2019, resolution = "20m", class = "sf")
+us_state_map <- tigris::shift_geometry(us_state_map, geoid_column = "GEOID", position = "below")
+# 郡级地图数据
+us_county_map <- tigris::counties(cb = T, year = 2019, resolution = "20m", class = "sf")
+us_county_map <- tigris::shift_geometry(us_county_map, geoid_column = "GEOID", position = "below")
+# 保存地图数据
+saveRDS(us_state_map, file = "Desktop/us_state_map.rds")
+saveRDS(us_county_map, file = "Desktop/us_county_map.rds")
+# 预览地图数据
+plot(st_geometry(us_state_map))
+plot(st_geometry(us_county_map))
