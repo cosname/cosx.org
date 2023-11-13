44ca84a6ce065ab399f5b54b5b4250998f8ece3a
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2023-11-13 23:04:00 +0800

    添加翻页提示框

--- static/js/shortcuts.js
+++ static/js/shortcuts.js
@@ -1,16 +1,19 @@
 // Press arrow keys to turn a page
 (() => {
-  // homepage
   document.addEventListener("keydown", (event) => {
     let currentPage, previousPage, nextPage;
-    try {
+    // Check if current page is homepage or post
+    // Code will fail on Chrome if this statement is not included inside AddEventListener and the user quickly press keys
+    if (document.querySelector(".article-list")) {
       currentPage = document.querySelector(".page-item.active");
       previousPage =
         currentPage.previousElementSibling.querySelector(".page-link");
       nextPage = currentPage.nextElementSibling.querySelector(".page-link");
-    } catch (error) {
+    } else if (document.querySelector("article")) {
       previousPage = document.querySelector(".nav-prev > a");
       nextPage = document.querySelector(".nav-next > a");
+    } else {
+      return;
     }
     const { key } = event;
     if (key === "ArrowLeft") {
@@ -19,7 +22,13 @@
       nextPage.click();
     }
   });
-  // todo:
-  //   shortcuts for first page and last page
-  //   add tooltip
+
+  // Add tooltips
+  const hint = "按左右键可翻页";
+  if (document.querySelector(".article-list")) {
+    document.querySelector(".pagination").setAttribute("title", hint);
+  } else if (document.querySelector("article")) {
+    document.querySelector(".nav-prev > a").setAttribute("title", hint);
+    document.querySelector(".nav-next > a").setAttribute("title", hint);
+  }
 })();
