0c5a4dfc96950d172c069ba81dc76a0051db58a9
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2023-11-18 02:33:14 +0800

    不要在代码块里面触发翻页

--- static/js/shortcuts.js
+++ static/js/shortcuts.js
@@ -1,10 +1,15 @@
 // Press arrow keys to turn a page
 (() => {
   document.addEventListener("keydown", (event) => {
+    // Don't turn a page inside a code block
+    if (document.activeElement !== document.body) {
+      return;
+    }
     let currentPage, previousPageLink, nextPageLink;
     // Check if current page is homepage or post
     // Code will fail on Chrome if this statement is not included inside AddEventListener and the user quickly press keys
     if (document.querySelector(".article-list")) {
+      // homepage
       currentPage = document.querySelector(".page-item.active");
       previousPageLink = currentPage.previousElementSibling
         .querySelector(".page-link")
@@ -13,6 +18,7 @@
         .querySelector(".page-link")
         .getAttribute("href");
     } else if (document.querySelector("article")) {
+      // post
       previousPageLink = document
         .querySelector(".nav-prev > a")
         .getAttribute("href");
@@ -43,4 +49,10 @@
     document.querySelector(".nav-prev > a").setAttribute("title", hint);
     document.querySelector(".nav-next > a").setAttribute("title", hint);
   }
+
+  // make <pre> focusable
+  const preElements = document.querySelectorAll("pre");
+  preElements.forEach((element) => {
+    element.setAttribute("tabindex", "0");
+  });
 })();
